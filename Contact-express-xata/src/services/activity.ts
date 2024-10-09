ae08e7509bfd16e431d4889f8fe2beed333391b6
symonmuchemi <simonkenya846@gmail.com>
2024-10-09 13:13:53 +0300

    update: adds get and patch routes
    fetches activity by id and update an activity respecitively

--- Contact-express-xata/src/services/activity.ts
+++ Contact-express-xata/src/services/activity.ts
@@ -26,8 +26,31 @@ router.post('/', async (req: Request, resp: Response) => {
         console.log(`Creating activity: ${req.body}`);
         resp.json(await xata.db.activity.create(req.body));
     } catch (err: any) {
-        resp.status(500).send({Error: err.toString()});
+        resp.status(500).send({ Error: err.toString() });
     }
-})
+});
+
+// get activity by its ID
+router.get('/:id', async (req: Request, resp: Response) => {
+    try {
+        const activityId = req.params.id;
+
+        if (typeof activityId === 'undefined') throw new Error('Id is undefined');
+        console.log(`Trying to query of ${activityId}`);
+        resp.json(await xata.db.activity.read(activityId));
+    } catch (err: any) {
+        resp.status(500).send({ Error: err.toString() });
+    }
+});
+
+// update an activity
+router.patch('/:id', async (req: Request, resp: Response) => {
+    try {
+        console.log(`Updating activity: ${req.params.id}`);
+        resp.json(await xata.db.activity.update(req.params.id, req.body));
+    } catch (err: any) {
+        resp.status(400).send({Error: err.toString()});
+    }
+});
 
 export default router;
