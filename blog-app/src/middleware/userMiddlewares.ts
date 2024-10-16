52a6f17f47325e2faf97bbfbc5f1d4681d1c53f3
symonmuchemi <simonkenya846@gmail.com>
2024-10-16 21:35:04 +0300

    update: implement thefetch user by id functionality

--- blog-app/src/middleware/userMiddlewares.ts
+++ blog-app/src/middleware/userMiddlewares.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from 'express';
-import { getAllUsers } from '../controllers/userController';
+import { fetchAllUsers, fetchUserById } from '../controllers/userController';
 
 export const getUsers = async (req: Request, resp: Response) => {
     try {
-        const users = await getAllUsers();
+        const users = await fetchAllUsers();
         resp.json(users)
     } catch (error: any) {
         resp.status(500).json({ Error: error.toString() });
+    }
+}
+
+export const getUserById = async (req: Request, resp: Response) => {
+    try {
+        const user = await fetchUserById(Number(req.params.id));
+        resp.json(user);
+    } catch (error: any) {
+        resp.status(400).json({ error: error.toString() });
     }
 }
