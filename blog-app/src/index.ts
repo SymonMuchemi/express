e712e915e97c71023886ba69cac793b60dd97a54
symonmuchemi <simonkenya846@gmail.com>
2024-10-16 15:37:51 +0300

    update: implements the user router

--- blog-app/src/index.ts
+++ blog-app/src/index.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from "@prisma/client";
-import express, { Express, Application } from "express";
+import express, { Application, Request, Response } from "express";
+import userRouter from "./routes/userRoutes";
 import dotenv from 'dotenv';
 
 dotenv.config()
 
-const prisma: PrismaClient = new PrismaClient();
 const app: Application = express();
 const PORT: string | number = process.env.PORT || 3500
 
-app.use(express.json())
+app.use(express.json());
 
+app.use('/api/users', userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
