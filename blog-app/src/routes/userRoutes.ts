9f411523b4607b367d454f92964e2edd6bfaaec2
symonmuchemi <simonkenya846@gmail.com>
2024-10-16 22:20:46 +0300

    update: defines route for creating a user

--- blog-app/src/routes/userRoutes.ts
+++ blog-app/src/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import express, { Router } from "express";
-import { getUsers, getUserById } from "../middleware/userMiddlewares";
+import { getUsers, getUserById, create } from "../middleware/userMiddlewares";
 import { asyncHandler } from "../utils/utils";
+import { userSchema } from "../validators/userValidator";
 
 const userRouter: Router = express.Router();
 
 userRouter.get('/', asyncHandler(getUsers));
 userRouter.get('/:id', asyncHandler(getUserById));
-
+userRouter.post('/', userSchema, asyncHandler(create))
 
 export default userRouter;
