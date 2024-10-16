f5f9ea6e2dc51b559686adbb198b7b7d8f1e2a66
symonmuchemi <simonkenya846@gmail.com>
2024-10-16 21:08:47 +0300

    update: create user creation post route

--- blog-app/src/routes/postRoutes.ts
+++ blog-app/src/routes/postRoutes.ts
@@ -1,9 +1,12 @@
 import express, { Router } from "express";
 import { asyncHandler } from "../utils/utils";
-import { getPosts, getPostById } from "../middleware/postMiddleware";
+import { getPosts, getPostById, create } from "../middleware/postMiddleware";
+import { postSchema } from "../validators/postValidator";
+
 const postRouter: Router = express.Router();
 
 postRouter.get('/', asyncHandler(getPosts));
 postRouter.get('/:id', asyncHandler(getPostById));
+postRouter.post('/', postSchema, asyncHandler(create));
 
 export default postRouter;
