e51955e05102fcb9065a948cbdbd36fa0c4de806
symonmuchemi <simonkenya846@gmail.com>
2024-10-16 23:02:36 +0300

    update: documents the getFeed middleware

--- blog-app/src/middleware/feedMiddleware.ts
+++ blog-app/src/middleware/feedMiddleware.ts
@@ -1,6 +1,16 @@
 import { Request, Response } from "express";
 import { fetchPublishedPosts } from "../controllers/feedController";
 
+/**
+ * Middleware to handle fetching the feed of published posts.
+ * 
+ * @param req - The request object.
+ * @param resp - The response object.
+ * 
+ * @returns A JSON response containing the feed of published posts or an error message.
+ * 
+ * @throws Will return a 400 status code with an error message if fetching the feed fails.
+ */
 export const getFeed = async (req: Request, resp: Response) => {
     try {
         const feed = await fetchPublishedPosts();
