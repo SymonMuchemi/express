d053ec4de732c5aac37b4e2db0f62552cffeeb40
symonmuchemi <simonkenya846@gmail.com>
2024-10-16 21:08:14 +0300

    update: creates create post middleware

--- blog-app/src/middleware/postMiddleware.ts
+++ blog-app/src/middleware/postMiddleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { fetchAllPosts, fetchPostById } from "../controllers/postController";
+import { validationResult } from "express-validator";
+import { fetchAllPosts, fetchPostById, createPost } from "../controllers/postController";
 
 /**
  * Retrieves all posts and sends them in the response.
@@ -38,5 +39,22 @@ export const getPostById = async (req: Request, resp: Response) => {
         resp.json(post)
     } catch (error: any) {
         resp.status(400).json({ error: error.toString() })
+    }
+}
+
+export const create = async (req: Request, resp: Response) => {
+    try {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return resp.status(400).json({ errors: errors.array() });
+        }
+
+        const post = req.body;
+        const result = await createPost(post);
+
+        resp.json({ message: 'Post created successfully!', post: result });
+    } catch (error: any) {
+        resp.status(400).json({ message: 'Error creating post!', details: error.toString() });
     }
 }
