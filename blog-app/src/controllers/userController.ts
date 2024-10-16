da0dbb6a49eec28430c05049cb4a573624ec7655
symonmuchemi <simonkenya846@gmail.com>
2024-10-16 21:33:31 +0300

    feat: implements fetch user by id logic

--- blog-app/src/controllers/userController.ts
+++ blog-app/src/controllers/userController.ts
@@ -8,8 +8,22 @@ const prisma: PrismaClient = new PrismaClient();
  *
  * @returns {Promise<Array>} A promise that resolves to an array of user objects.
  */
-export const getAllUsers = async () => {
+export const fetchAllUsers = async () => {
     const users: User[] = await prisma.user.findMany();
 
     return users
+}
+
+/**
+ * Fetches a user by their unique ID.
+ *
+ * @param id - The unique identifier of the user to fetch.
+ * @returns A promise that resolves to the user object if found, or null if not found.
+ */
+export const fetchUserById = async (id: number) => {
+    const user: User | null = await prisma.user.findUnique({
+        where: { id: id }
+    });
+
+    return user;
 }
