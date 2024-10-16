187f25bc2c9c0bf1829303860c7d1d475dd1bf17
symonmuchemi <simonkenya846@gmail.com>
2024-10-16 22:45:53 +0300

    feat/update: creates post publishing middleware and adds doces to create middleware

--- blog-app/src/middleware/postMiddleware.ts
+++ blog-app/src/middleware/postMiddleware.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
-import { fetchAllPosts, fetchPostById, createPost } from "../controllers/postController";
+import {
+    fetchAllPosts,
+    fetchPostById,
+    createPost,
+    publishPost
+} from "../controllers/postController";
 
 /**
  * Retrieves all posts and sends them in the response.
@@ -41,6 +46,19 @@ export const getPostById = async (req: Request, resp: Response) => {
     }
 }
 
+/**
+ * Handles the creation of a new post.
+ * 
+ * This middleware function validates the request for any errors, and if none are found,
+ * it proceeds to create a new post using the provided data in the request body.
+ * 
+ * @param req - The request object containing the post data.
+ * @param resp - The response object used to send back the appropriate response.
+ * 
+ * @returns A JSON response indicating the success or failure of the post creation.
+ * 
+ * @throws Will return a 400 status code with error details if post creation fails.
+ */
 export const create = async (req: Request, resp: Response) => {
     try {
         const errors = validationResult(req);
@@ -55,5 +73,23 @@ export const create = async (req: Request, resp: Response) => {
         resp.json({ message: 'Post created successfully!', post: result });
     } catch (error: any) {
         resp.status(400).json({ message: 'Error creating post!', details: error.toString() });
+    }
+}
+
+/**
+ * Publishes a blog post based on the provided post ID in the request parameters.
+ * 
+ * @param req - The request object containing the post ID in the parameters.
+ * @param resp - The response object used to send the response back to the client.
+ * 
+ * @returns A JSON response with a success message and the published post data, 
+ *          or an error message if the publishing fails.
+ */
+export const publish = async (req: Request, resp: Response) => {
+    try {
+        const post = publishPost(Number(req.params.id));
+        resp.json({ message: 'Post published!' })
+    } catch (error: any) {
+        resp.status(400).json({ error: error.toString() })
     }
 }
