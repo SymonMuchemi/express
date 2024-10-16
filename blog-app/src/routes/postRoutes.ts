4154b5d02162be8212d592ac41c1dc527580903b
symonmuchemi <simonkenya846@gmail.com>
2024-10-16 22:46:21 +0300

    update: registers a post publish route

--- blog-app/src/routes/postRoutes.ts
+++ blog-app/src/routes/postRoutes.ts
@@ -1,12 +1,13 @@
 import express, { Router } from "express";
 import { asyncHandler } from "../utils/utils";
-import { getPosts, getPostById, create } from "../middleware/postMiddleware";
 import { postSchema } from "../validators/postValidator";
+import { getPosts, getPostById, create, publish } from "../middleware/postMiddleware";
 
 const postRouter: Router = express.Router();
 
 postRouter.get('/', asyncHandler(getPosts));
 postRouter.get('/:id', asyncHandler(getPostById));
 postRouter.post('/', postSchema, asyncHandler(create));
+postRouter.put('/publish/:id', asyncHandler(publish));
 
 export default postRouter;
