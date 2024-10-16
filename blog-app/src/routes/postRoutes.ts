6bdea56acbe0c5198453917ad9d961cb7368cff8
symonmuchemi <simonkenya846@gmail.com>
2024-10-16 23:12:11 +0300

    update: register delete post in a route

--- blog-app/src/routes/postRoutes.ts
+++ blog-app/src/routes/postRoutes.ts
@@ -1,7 +1,7 @@
 import express, { Router } from "express";
 import { asyncHandler } from "../utils/utils";
 import { postSchema } from "../validators/postValidator";
-import { getPosts, getPostById, create, publish } from "../middleware/postMiddleware";
+import { getPosts, getPostById, create, publish, deleteMiddleware } from "../middleware/postMiddleware";
 
 const postRouter: Router = express.Router();
 
@@ -9,5 +9,6 @@ postRouter.get('/', asyncHandler(getPosts));
 postRouter.get('/:id', asyncHandler(getPostById));
 postRouter.post('/', postSchema, asyncHandler(create));
 postRouter.put('/publish/:id', asyncHandler(publish));
+postRouter.delete('/:id', asyncHandler(deleteMiddleware));
 
 export default postRouter;
