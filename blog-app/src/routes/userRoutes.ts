5ce2fd4ae36e8c5c1b52dba6cddf4023b0a7b915
symonmuchemi <simonkenya846@gmail.com>
2024-10-16 15:42:44 +0300

    update: wraps getUser middleware inside async handle for better error handling

--- blog-app/src/routes/userRoutes.ts
+++ blog-app/src/routes/userRoutes.ts
@@ -3,12 +3,20 @@ import { getUsers } from "../middleware/userMiddlewares";
 
 const userRouter: Router = express.Router();
 
+/**
+ * A higher-order function that wraps an asynchronous route handler function
+ * and ensures that any errors are passed to the next middleware.
+ *
+ * @param fn - The asynchronous route handler function to be wrapped.
+ * @returns A new function that wraps the provided function and catches any errors,
+ *          passing them to the next middleware.
+ */
 const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
     (req: Request, res: Response, next: NextFunction) => {
         Promise.resolve(fn(req, res, next)).catch(next);
     };
 
-userRouter.get('/', getUsers);
+userRouter.get('/', asyncHandler(getUsers));
 
 
 export default userRouter;
