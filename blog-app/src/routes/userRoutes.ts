0839677003fc5ca8c44787419d23c9abafc447ad
symonmuchemi <simonkenya846@gmail.com>
2024-10-16 21:35:26 +0300

    update: creates route for fetching user by id

--- blog-app/src/routes/userRoutes.ts
+++ blog-app/src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
-import { getUsers } from "../middleware/userMiddlewares";
+import { getUsers, getUserById } from "../middleware/userMiddlewares";
 import { asyncHandler } from "../utils/utils";
 
 const userRouter: Router = express.Router();
 
 userRouter.get('/', asyncHandler(getUsers));
+userRouter.get('/:id', asyncHandler(getUserById));
 
 
 export default userRouter;
